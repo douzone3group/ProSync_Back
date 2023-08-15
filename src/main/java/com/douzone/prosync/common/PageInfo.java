13c54d2d871050d2fb42180a0d3c171c6fed095e
ShimChang <95128916+ShimChang@users.noreply.github.com>
2023-08-15 20:29:31 +0900

    feat:project 기본 CRUD 구현

---
+++ src/main/java/com/douzone/prosync/common/PageInfo.java
@@ -0,0 +1,15 @@
+package com.douzone.prosync.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PageInfo {
+
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+
+}
---
+++ src/main/java/com/douzone/prosync/common/PageResponseDto.java
@@ -0,0 +1,20 @@
+package com.douzone.prosync.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class PageResponseDto<T> {
+
+    private List<T> data;
+    private PageInfo pageInfo;
+
+    public PageResponseDto(List<T> data, Page page) {
+        this.data = data;
+        pageInfo = new PageInfo(page.getNumber() + 1, page.getSize(), page.getTotalElements(), page.getTotalPages());
+    }
+}
