08a8b30ffb9e0b8e5c71e828265671af59f3aa84
kangwook <rlarkddnr1686@naver.com>
2023-08-18 19:41:05 +0900

    refactor: Security matcher 및 Jwt 필터 수정

--- src/main/java/com/douzone/prosync/security/jwt/JwtFilter.java
+++ src/main/java/com/douzone/prosync/security/jwt/JwtFilter.java
@@ -50,9 +50,8 @@ public class JwtFilter extends GenericFilterBean {
                 log.info("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
             } else {
                 log.info("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
-                throw new NoJwtTokenException("JWT 토큰이 없습니다. Refresh 토큰을 확인하겠습니다.");
             }
-        } catch (NoJwtTokenException | ExpiredTokenException e) {
+        } catch (ExpiredTokenException e) {
 
             try {
                 // redis에 refresh 토큰이 존재할 경우 새 토큰 발급한다.
