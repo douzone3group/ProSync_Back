09d5b9ce3211adfcdb2265a3ea794bc917aeefb2
ShimChang <95128916+ShimChang@users.noreply.github.com>
2023-08-18 14:43:06 +0900

    feat:project API  명세 추가 (#14)
    * feat:project 생성 기능 추가
    
    * feat:project 기본 CRUD 구현
    
    * feat:project 기본 CRUD 구현

--- src/main/java/com/douzone/prosync/project/dto/ProjectResponse.java
+++ src/main/java/com/douzone/prosync/project/dto/ProjectResponse.java
@@ -1,6 +1,7 @@
 package com.douzone.prosync.project.dto;
 
 import com.douzone.prosync.project.entity.Project;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 
@@ -10,17 +11,35 @@ public class ProjectResponse {
     @AllArgsConstructor
     @Builder
     public static class GetProjectResponse{
+
+        @ApiModelProperty(example = "프로젝트 아이디")
         private Integer projectId;
+
+        @ApiModelProperty(example = "프로젝트 이름")
         private String name;
+
+        @ApiModelProperty(example = "프로젝트 인트로")
         private String intro;
 
+        @ApiModelProperty(example = "프로젝트 생성 날짜")
         private String createdAt;
+
+        @ApiModelProperty(example = "프로젝트 시작 날짜")
         private String startDate;
+
+        @ApiModelProperty(example = "프로젝트 종료 날짜")
         private String endDate;
+
+        @ApiModelProperty(example = "프로젝트 수정 날짜")
         private String modifiedAt;
 
+        @ApiModelProperty(example = "프로젝트 진행도")
         private Float progress;
+
+        @ApiModelProperty(example = "프로젝트 공개여부")
         private Boolean publicyn;
+
+        @ApiModelProperty(example = "프로젝트 이미지")
         private String projectImage;
 
         public static GetProjectResponse of(Project project) {
@@ -43,13 +62,23 @@ public class ProjectResponse {
     @Getter
     @AllArgsConstructor
     public static class GetProjectsResponse{
+
+        @ApiModelProperty(example = "프로젝트 아이디")
         private Integer projectId;
+
+        @ApiModelProperty(example = "프로젝트 이름")
         private String name;
 
+        @ApiModelProperty(example = "프로젝트 시작 날짜")
         private String startDate;
+
+        @ApiModelProperty(example = "프로젝트 종료 날짜")
         private String endDate;
 
+        @ApiModelProperty(example = "프로젝트 진행도")
         private Float progress;
+
+        @ApiModelProperty(example = "프로젝트 이미지")
         private String projectImage;
         // TODO: 회원 정보 추가
 
@@ -70,6 +99,8 @@ public class ProjectResponse {
     @Setter
     @AllArgsConstructor
     public static class SimpleResponse {
+
+        @ApiModelProperty(example = "프로젝트 아이디")
         private Integer projectId;
     }
 
