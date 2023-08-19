f5b21e0ce3f3791c1cdbeaf0973ca36bd67d385f
kimmj13 <110887976+kimmj13@users.noreply.github.com>
2023-08-19 19:11:23 +0900

    feat: 업무 담당자 추가 삭제 기능 추가, 프로젝트 생성시 업무 상태 추가 (#17)
    * feat: 업무 기본 CRUD 추가
    
    * feat: 업무와 프로젝트 연관 관계 설정 #2
    
    * refactor: 기존 코드 수정
    - 페이지네이션 로직 변경
    - service 인터페이스 추가
    
    * refactor: 업무 코드 리팩토링 #2
    - entity -> dto 전달 방식으로 변경 및 DTO 파일 분할
    - 업무 상태 로직 변경으로 불필요 파일 삭제 (TaskMapper, TaskStatus)
    - swagger api 코드 추가
    - transaction readonly 속성 추가
    - entity - setter 제거
    
    * feat: task swagger api 추가
    
    * feat: task status CRUD 추가
    
    * feat: task와 task status 연관관계 설정, project 생성시 기본 task status 4가지 설정 #2
    
    * style: 스타일 변경 #2
    - resource - mapper 폴더 위치 변경
    - xml : sql문 대문자 변경
    - mybatis mapper -> repository DI 않고 service 에서 mapper 바로 사용하도록 변경
    
    * feat: member_task 관련 업무 담당자 추가/삭제 기능 추가
    
    * feat: 프로젝트 생성시 기본 업무 상태 추가 #7

--- src/main/java/com/douzone/prosync/task/dto/response/GetTaskResponse.java
+++ src/main/java/com/douzone/prosync/task/dto/response/GetTaskResponse.java
@@ -1,6 +1,5 @@
 package com.douzone.prosync.task.dto.response;
 
-import com.douzone.prosync.task.entity.Task;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
@@ -29,26 +28,22 @@ public class GetTaskResponse {
         @ApiModelProperty(value = "종료일자", example = "2023-10-31")
         private String endDate;
 
-        @ApiModelProperty(value = "업무상태", example = "TODO")
-        private String taskStatus;
-
         @ApiModelProperty(value = "생성일자", example = "2023-10-01")
         private String createdAt;
 
         @ApiModelProperty(value = "최근수정일자", example = "2023-10-01")
         private String modifiedAt;
 
-        public static GetTaskResponse of(Task task) {
-            return GetTaskResponse.builder()
-                    .taskId(task.getTaskId())
-                    .classification(task.getClassification())
-                    .title(task.getTitle())
-                    .detail(task.getDetail())
-                    .startDate(task.getStartDate())
-                    .endDate(task.getEndDate())
-                    .taskStatus(task.getTaskStatus())
-                    .createdAt(task.getCreatedAt().toString())
-                    .modifiedAt(task.getModifiedAt().toString()).build();
+        @ApiModelProperty(value = "업무상태식별자", example = "1")
+        private Integer taskStatusId;
 
-    }
+        @ApiModelProperty(value = "색상", example = "#000000")
+        private String color;
+
+        @ApiModelProperty(value = "업무상태", example = "TODO")
+        private String taskStatus;
+
+        @ApiModelProperty(value = "프로젝트식별자", example = "1")
+        private Integer projectId;
+
 }
---
+++ src/main/java/com/douzone/prosync/task_status/controller/TaskStatusController.java
@@ -0,0 +1,113 @@
+package com.douzone.prosync.task_status.controller;
+
+import com.douzone.prosync.common.SingleResponseDto;
+import com.douzone.prosync.security.auth.MemberDetails;
+import com.douzone.prosync.task_status.dto.TaskStatusDto;
+import com.douzone.prosync.task_status.service.TaskStatusService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+@Validated
+@Tag(name = "task_status", description = "업무 상태 API")
+public class TaskStatusController {
+
+    private final TaskStatusService taskStatusService;
+
+    /**
+     * 특정 프로젝트 업무 상태 생성
+     */
+    @PostMapping("/projects/{project-id}/task-status")
+    @Operation(summary = "업무 상태 생성", description = "특정 프로젝트의 업무 상태를 생성합니다.", tags = "task_status")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "successfully retrieved", response = TaskStatusDto.SimpleResponseDto.class),
+            @ApiResponse(code = 404, message = "project not found"),
+            @ApiResponse(code = 500, message = "server error"),
+    })
+    public ResponseEntity<SingleResponseDto<TaskStatusDto.SimpleResponseDto>> postTaskStatus(@Parameter(description = "프로젝트식별자", required = true, example = "1") @PathVariable("project-id") @Positive Integer projectId,
+                                                                                             @RequestBody @Valid TaskStatusDto.PostDto requestBody,
+                                                                                             @Parameter(hidden = true) @ApiIgnore @AuthenticationPrincipal MemberDetails memberDetails) {
+        Integer taskStatusId = taskStatusService.createTaskStatus(projectId, requestBody, memberDetails.getMemberId());
+        return new ResponseEntity(new SingleResponseDto(new TaskStatusDto.SimpleResponseDto(taskStatusId)), HttpStatus.CREATED);
+    }
+
+    /**
+     * 업무 상태 수정
+     */
+    @PatchMapping("/task-status/{task-status-id}")
+    @Operation(summary = "업무 상태 수정", description = "업무 상태를 수정합니다.", tags = "task_status")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "successfully retrieved", response = TaskStatusDto.SimpleResponseDto.class),
+            @ApiResponse(code = 404, message = "task_status_not_found"),
+            @ApiResponse(code = 500, message = "server error"),
+    })
+    public ResponseEntity<SingleResponseDto<TaskStatusDto.SimpleResponseDto>> patchTaskStatus(@Parameter(description = "업무상태식별자", required = true, example = "1") @PathVariable("task-status-id") @Positive Integer taskStatusId,
+                                                                                              @RequestBody @Valid TaskStatusDto.PatchDto requestBody,
+                                                                                              @Parameter(hidden = true) @ApiIgnore @AuthenticationPrincipal MemberDetails memberDetails) {
+        taskStatusService.updateTaskStatus(taskStatusId, requestBody, memberDetails.getMemberId());
+        return new ResponseEntity(new SingleResponseDto(new TaskStatusDto.SimpleResponseDto(taskStatusId)), HttpStatus.OK);
+    }
+
+    /**
+     * 업무 상태 삭제
+     */
+    @DeleteMapping("/task-status/{task-status-id}")
+    @Operation(summary = "업무 상태 삭제", description = "업무 상태를 삭제합니다.", tags = "task_status")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "successfully retrieved"),
+            @ApiResponse(code = 404, message = "task_status_not_found"),
+            @ApiResponse(code = 500, message = "server error"),
+    })
+    public ResponseEntity deleteTaskStatus(@Parameter(description = "업무상태식별자", required = true, example = "1") @PathVariable("task-status-id") @Positive Integer taskStatusId,
+                                           @Parameter(hidden = true) @ApiIgnore @AuthenticationPrincipal MemberDetails memberDetails) {
+        taskStatusService.deleteTaskStatus(taskStatusId, memberDetails.getMemberId());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    /**
+     * 프로젝트에 대한 모든 업무상태 조회
+     */
+    @GetMapping("/projects/{project-id}/task-status")
+    @Operation(summary = "특정 프로젝트의 업무 상태 목록 조회", description = "특정 프로젝트의 업무 상태 목록을 전체 조회합니다. ", tags = "task_status")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "successfully retrieved", response = TaskStatusDto.GetResponseDto.class),
+            @ApiResponse(code = 404, message = "task_status_not_found"),
+            @ApiResponse(code = 500, message = "server error"),
+    })
+    public ResponseEntity<SingleResponseDto<List<TaskStatusDto.GetResponseDto>>> getProjectTaskStatus(@Parameter(description = "프로젝트 식별자", required = true, example = "1") @PathVariable("project-id") @Positive Integer projectId,
+                                                                                                      @Parameter(description = "보여짐 체크한 업무상태 조회", required = false, example = "true") @RequestParam(required = false) boolean isActive,
+                                                                                                      @Parameter(hidden = true) @ApiIgnore @AuthenticationPrincipal MemberDetails memberDetails) {
+        return new ResponseEntity(new SingleResponseDto(taskStatusService.getTaskStatusByProject(projectId, isActive, memberDetails.getMemberId())), HttpStatus.OK);
+    }
+
+    /**
+     * 업무상태 1건 조회
+     */
+    @GetMapping("/task-status/{task-status-id}")
+    @Operation(summary = "업무 상태 조회", description = "업무상태식별자를 통해 업무 상태를 조회합니다.", tags = "task_status")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "successfully retrieved", response = TaskStatusDto.GetResponseDto.class),
+            @ApiResponse(code = 404, message = "task_status_not_found"),
+            @ApiResponse(code = 500, message = "server error"),
+    })
+    public ResponseEntity<SingleResponseDto<TaskStatusDto.GetResponseDto>> getTaskStatus(@Parameter(description = "업무상태식별자", required = true, example = "1") @PathVariable("task-status-id") @Positive Integer taskStatusId,
+                                                                                         @Parameter(hidden = true) @ApiIgnore @AuthenticationPrincipal MemberDetails memberDetails) {
+        return new ResponseEntity(new SingleResponseDto<>(taskStatusService.getTaskStatus(taskStatusId, memberDetails.getMemberId())), HttpStatus.OK);
+    }
+}
