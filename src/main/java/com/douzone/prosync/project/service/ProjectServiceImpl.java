2b30f03213f5f630e500a23dd3b3155c4393476f
ShimChang <95128916+ShimChang@users.noreply.github.com>
2023-08-23 20:03:46 +0900

    feat:project 종료일 순 정렬, 이름 검색 기능 추가 (#25)
    * feat:project 생성 기능 추가
    
    * feat:project 기본 CRUD 구현
    
    * feat:project 기본 CRUD 구현
    
    * feat:project 유효성 로직 추가  #12
    
    * feat:comment 댓글 기본 CRUD #18
    
    * style:불필요한 파일 제거
    
    * feat:comment 댓글 CRUD 기능 구현 #18
    
    * feat:comment 프로젝트 end_date 순 정렬, 검색 기능 구현 #12
    
    * feat:comment 프로젝트 end_date 순 정렬, 검색 기능 구현 #12

--- src/main/java/com/douzone/prosync/project/service/ProjectServiceImpl.java
+++ src/main/java/com/douzone/prosync/project/service/ProjectServiceImpl.java
@@ -56,4 +56,19 @@ public class ProjectServiceImpl implements ProjectService{
     public Page<Project> findProjectList(Pageable pageable) {
         return projectJpaRepository.findAllByIsDeletedNull(pageable);
     }
+
+    // 프로젝트 종료 임박 순 정렬
+    public Page<Project> findProjectsSortedByEndDateAsc(Pageable pageable) {
+        return projectJpaRepository.findAllByOrderByEndDateAsc(pageable);
+    }
+
+    // 프로젝트 종료 임박 순 반대 정렬
+    public Page<Project> findProjectsSortedByEndDateDesc(Pageable pageable) {
+        return projectJpaRepository.findAllByOrderByEndDateDesc(pageable);
+    }
+
+    //프로젝트 이름 검색 필터
+    public Page<Project> findProjectsByName(String name, Pageable pageable) {
+        return projectJpaRepository.findByNameContaining(name, pageable);
+    }
 }
