24bd749681e566c4419c9265cb97fc482dd5de16
kimmj13 <110887976+kimmj13@users.noreply.github.com>
2023-08-23 20:24:50 +0900

    fix: 컨트롤러 코드 수정

--- src/main/java/com/douzone/prosync/project/controller/ProjectController.java
+++ src/main/java/com/douzone/prosync/project/controller/ProjectController.java
@@ -43,15 +43,6 @@ public class ProjectController {
     @PostMapping
     @ApiOperation(value = "프로젝트 생성",notes = "프로젝트를 생성한다" ,tags = "project")
     @ApiResponses(value = {
-<<<<<<< HEAD
-            @ApiResponse(code = 201, message = "successfully retrieved", response = ProjectResponse.SimpleResponse.class),
-            @ApiResponse(code = 404, message = "project not found"),
-            @ApiResponse(code = 500, message = "server error"),
-    })
-    public ResponseEntity createProject(@RequestBody @Valid ProjectRequest.PostDto dto) {
-        Integer projectId=projectService.save(dto);
-        return new ResponseEntity(new ProjectResponse.SimpleResponse(projectId), HttpStatus.CREATED);
-=======
             @ApiResponse(code = 201, message = "successfully retrieved", response = ProjectSimpleResponse.class),
             @ApiResponse(code = 404, message = "project not found"),
             @ApiResponse(code = 500, message = "server error"),
@@ -59,18 +50,13 @@ public class ProjectController {
     public ResponseEntity createProject(@RequestBody @Valid ProjectPostDto dto) {
         Integer projectId = projectService.save(dto);
         return new ResponseEntity(new ProjectSimpleResponse(projectId), HttpStatus.CREATED);
->>>>>>> 2b30f03213f5f630e500a23dd3b3155c4393476f
     }
 
     // 프로젝트 단일 조회
     @GetMapping("/{project-id}")
     @ApiOperation(value = "프로젝트 단일 조회", notes = "프로젝트를 단일 조회 한다", tags = "project")
     @ApiResponses(value = {
-<<<<<<< HEAD
-            @ApiResponse(code = 200, message = "successfully retrieved", response = ProjectResponse.GetProjectResponse.class),
-=======
             @ApiResponse(code = 200, message = "successfully retrieved", response = GetProjectResponse.class),
->>>>>>> 2b30f03213f5f630e500a23dd3b3155c4393476f
             @ApiResponse(code = 404, message = "project not found"),
             @ApiResponse(code = 500, message = "Internal Server Error"),
     })
@@ -88,22 +74,13 @@ public class ProjectController {
     @GetMapping
     @ApiOperation(value = "프로젝트 전체 조회",notes = "프로젝트를 전체 조회 한다",tags = "project")
     @ApiResponses(value = {
-<<<<<<< HEAD
-            @ApiResponse(code = 200, message = "successfully retrieved",response = ProjectResponse.GetProjectsResponse.class),
-=======
             @ApiResponse(code = 200, message = "successfully retrieved",response = GetProjectsResponse.class),
->>>>>>> 2b30f03213f5f630e500a23dd3b3155c4393476f
             @ApiResponse(code = 404, message = "project not found"),
             @ApiResponse(code = 500, message = "Internal Server Error"),
     })
     @ApiImplicitParams({
             @ApiImplicitParam(name = "page", dataType = "integer", paramType = "query", value = "조회할 페이지 번호", defaultValue = "1", example = "1"),
             @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query", value = "한페이지에 보여질 요소 개수", defaultValue = "10", example = "20")})
-<<<<<<< HEAD
-    public ResponseEntity<PageResponseDto<ProjectResponse.GetProjectsResponse>> getProjectList(
-            @Parameter(hidden = true) @ApiIgnore @PageableDefault (size=8, sort="projectId", direction = Sort.Direction.DESC) Pageable pageable) {
-        Page<Project> pages = projectService.findProjectList(pageable);
-=======
     public ResponseEntity<PageResponseDto<GetProjectsResponse>> getProjectList(
             @RequestParam(required = false) String sortBy,
             @RequestParam(required = false) String name,
@@ -123,7 +100,6 @@ public class ProjectController {
         }
 
 //        Page<Project> pages = projectService.findProjectList(pageable);
->>>>>>> 2b30f03213f5f630e500a23dd3b3155c4393476f
         List<Project> projects = pages.getContent();
         List<GetProjectsResponse> projectsResponse
                 = projects.stream().map(GetProjectsResponse::of).collect(Collectors.toList());
@@ -136,20 +112,12 @@ public class ProjectController {
     @PatchMapping("/{project-id}")
     @ApiOperation(value = "프로젝트 수정", notes = "프로젝트를 수정한다", tags = "project")
     @ApiResponses(value = {
-<<<<<<< HEAD
-            @ApiResponse(code = 200, message = "successfully retrieved", response = ProjectResponse.SimpleResponse.class),
-=======
             @ApiResponse(code = 200, message = "successfully retrieved", response = ProjectSimpleResponse.class),
->>>>>>> 2b30f03213f5f630e500a23dd3b3155c4393476f
             @ApiResponse(code = 404, message = "project not found"),
             @ApiResponse(code = 500, message = "Internal Server Error"),
     })
     public ResponseEntity updateProject(@Parameter(description = "프로젝트 식별자", required = true, example = "1")
-<<<<<<< HEAD
-                                        @PathVariable("project-id") Integer projectId, @RequestBody @Valid ProjectRequest.PatchDto dto) {
-=======
                                         @PathVariable("project-id") Integer projectId, @RequestBody @Valid ProjectPatchDto dto) {
->>>>>>> 2b30f03213f5f630e500a23dd3b3155c4393476f
         dto.setProjectId(projectId);
         projectService.update(dto);
         return new ResponseEntity(new ProjectSimpleResponse(projectId), HttpStatus.OK);
@@ -159,11 +127,7 @@ public class ProjectController {
     @DeleteMapping("/{project-id}")
     @ApiOperation(value = "프로젝트 삭제", notes = "프로젝트를 소프트 삭제 한다", tags = "project")
     @ApiResponses(value = {
-<<<<<<< HEAD
-            @ApiResponse(code = 204, message = "successfully retrieved", response = ProjectResponse.SimpleResponse.class),
-=======
             @ApiResponse(code = 204, message = "successfully retrieved", response = ProjectSimpleResponse.class),
->>>>>>> 2b30f03213f5f630e500a23dd3b3155c4393476f
             @ApiResponse(code = 404, message = "project not found"),
             @ApiResponse(code = 500, message = "Internal Server Error"),
     })
