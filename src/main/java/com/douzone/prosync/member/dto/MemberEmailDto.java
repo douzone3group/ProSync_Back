d6b3264778bb1b4f12c3194c00decf9d77033760
d0nghee <hdh960326@gmail.com>
2023-09-15 12:36:23 +0900

    feat : 회원가입 로직 중 이메일 중복 확인 기능 분리

---
+++ src/main/java/com/douzone/prosync/member/dto/MemberEmailDto.java
@@ -0,0 +1,12 @@
+package com.douzone.prosync.member.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MemberEmailDto {
+    private String email;
+}
