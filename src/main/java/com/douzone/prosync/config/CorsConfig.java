fac9a4321bbc3ff26140252b57d3253c2a7ef990
kangwook <rlarkddnr1686@naver.com>
2023-10-11 16:13:09 +0900

    refactor: ProjectMapper admin 쿼리 수정

--- src/main/java/com/douzone/prosync/config/CorsConfig.java
+++ src/main/java/com/douzone/prosync/config/CorsConfig.java
@@ -1,26 +1,3 @@
-//package com.douzone.prosync.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
-//
-//@Configuration
-//public class CorsConfig {
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOriginPattern("*");
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//
-//        source.registerCorsConfiguration("/**", config);
-//        return new CorsFilter(source);
-//    }
-//}
 package com.douzone.prosync.config;
 
 import org.springframework.context.annotation.Bean;
@@ -40,12 +17,11 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-//        config.addAllowedOriginPattern("*");
-        config.addAllowedOriginPattern(FRONT_SERVER_HOST);
+        config.addAllowedOriginPattern("*");
+//        config.addAllowedOriginPattern(FRONT_SERVER_HOST);
         config.setExposedHeaders(List.of("Authorization", "Refresh"));
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
-        config.setExposedHeaders(List.of("Authorization","Refresh"));
 
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
